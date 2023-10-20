70827d6b1e4c66c08d14bffcf1238aac741228bb
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2023-10-20 10:57:51 -0500

    first commit

---
+++ # For this project, you and your partner.py
@@ -0,0 +1,3 @@
+# For this project, you and your partner(s) will work to create a program that has a "player" and a "computer" adversary.
+# The computer should randomly choose its decision based on a list of actions it can take ("Rock", "Paper" or "Scissors"). 
+# The player should then have a chance to input their decision. 
