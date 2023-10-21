713bf90e6c49e9bb8735699cb711831a468c98cf
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2023-10-21 10:44:46 -0500

    RPS

--- RPS game.py
+++ RPS game.py
@@ -4,16 +4,6 @@
 # The computer should randomly choose its decision based on a list of actions it can take ("Rock", "Paper" or "Scissors"). 
 # The player should then have a chance to input their decision. 
 
-
-def rps(p1, p2):
-    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
-    if beats [p1] == p2:
-        return "Player 1 won!"
-    if beats [p2] == p1:
-        return "Player 2 won!"
-    return "Draw!"
-
-def rock_paper_scissor(p, c):
   #  if rock > scissor = rock (wins)
   #  if rock < paper = paper (wins)
   #  if scissor < rock = rock (wins)
@@ -24,4 +14,23 @@ def rock_paper_scissor(p, c):
   # input("What is your decision?")
   # Computer vs. Player (us)
   # randomgenerator
-  
+
+import random
+def rock_paper_scissor():
+    user = input("What is your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'It is a tie!'
+    else:
+        return 'You lose! Play again!'
+    
+def wins(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+    
+print(rock_paper_scissor())
+
+
